4616c35a15050dff57416a55a1b37a6cc87a1a25
farkuy <darkuy@mail.ru>
2025-05-21 22:40:13 +0300

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as process from 'process';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import 'reflect-metadata';
 import * as cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 8080;
@@ -12,6 +13,15 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
   app.use(cookieParser());
+
+  const config = new DocumentBuilder()
+    .setTitle('Bridge')
+    .setDescription('Тестовый проект для риал тайма')
+    .setVersion('1.0')
+    .addTag('bridge')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(PORT, () => console.log(`Server start on ${PORT} port`));
 }
